ed1cd2af90727865d649d9a8011293d45288ac8e
si1991Van <sivannguyen1991@gmail.com>
2018-04-03 14:49:15 +0700

    update UI

--- app/src/main/java/haiphat/com/bds/nghetuvan/viewmodel/news/NewsViewModel.kt
+++ app/src/main/java/haiphat/com/bds/nghetuvan/viewmodel/news/NewsViewModel.kt
@@ -2,9 +2,6 @@ package haiphat.com.bds.nghetuvan.viewmodel.news
 
 import haiphat.com.bds.nghetuvan.models.news.NewsResponse
 
-/**
- * Created by HUONG HA^P on 3/28/2018.
- */
 class NewsViewModel {
     fun getItemNews(onSuccess : (ArrayList<NewsResponse>) ->Unit, onFailed : (String?) -> Unit) {
         onSuccess(mockDataItem())
