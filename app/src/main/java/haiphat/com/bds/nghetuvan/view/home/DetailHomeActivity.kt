3306d068917d3e7cbb5ca41baa366ec7c2375540
si1991Van <sivannguyen1991@gmail.com>
2018-06-25 12:02:32 +0700

    create desgin detail home

--- app/src/main/java/haiphat/com/bds/nghetuvan/view/home/DetailHomeActivity.kt
+++ app/src/main/java/haiphat/com/bds/nghetuvan/view/home/DetailHomeActivity.kt
@@ -2,19 +2,45 @@ package haiphat.com.bds.nghetuvan.view.home
 
 import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.design.widget.AppBarLayout
 import android.view.View
 import haiphat.com.bds.nghetuvan.R
 import haiphat.com.bds.nghetuvan.databinding.ActivityDetailHomeBinding
+import haiphat.com.bds.nghetuvan.utils.CommonUtil
 import haiphat.com.bds.nghetuvan.view.BaseActivity
 
 class DetailHomeActivity : BaseActivity() {
     private lateinit var dataBindingDetailHome : ActivityDetailHomeBinding
     override fun getContentView(): View {
         dataBindingDetailHome = DataBindingUtil.inflate(layoutInflater, R.layout.activity_detail_home, null, false)
+        setSupportActionBar(dataBindingDetailHome.toolbar)
+
+        dataBindingDetailHome.appBar.addOnOffsetChangedListener(AppBarLayout.OnOffsetChangedListener { appBar, verticalOffset ->
+            when (verticalOffset) {
+                0 -> {
+                    dataBindingDetailHome.tvTitle.visibility = View.GONE
+                }else -> {
+                    if (Math.abs(verticalOffset) >= appBar.totalScrollRange) {
+                        dataBindingDetailHome.tvTitle.visibility = View.VISIBLE
+                    } else {
+                        dataBindingDetailHome.tvTitle.visibility = View.GONE
+                    }
+                }
+            }
+        })
+
+        dataBindingDetailHome.rippleBack.setOnRippleCompleteListener {
+            onBackPressed()
+        }
+        dataBindingDetailHome.rippleSetting.setOnRippleCompleteListener {
+            CommonUtil.shareAppLinkViaFacebook(this,"https://developers.facebook.com")
+        }
+
         return dataBindingDetailHome.root
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHeaderVisibility(View.GONE)
     }
 }
