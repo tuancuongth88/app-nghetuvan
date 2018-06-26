458aa13fdaf659ea9749173c1342d0fd4571de6a
si1991Van <sivannguyen1991@gmail.com>
2018-06-26 11:15:36 +0700

    update code

---
+++ app/src/main/java/haiphat/com/bds/nghetuvan/adapter/home/DocumentAdapter.kt
@@ -0,0 +1,38 @@
+package haiphat.com.bds.nghetuvan.adapter.home
+
+import android.databinding.DataBindingUtil
+import android.databinding.ViewDataBinding
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import haiphat.com.bds.nghetuvan.R
+import haiphat.com.bds.nghetuvan.models.home.HomeCategoryResponse
+import kotlinx.android.synthetic.main.item_document.view.*
+
+/**
+ * Created by HUONG HA^P on 6/20/2018.
+ */
+class DocumentAdapter(private val listHomePage: ArrayList<HomeCategoryResponse>, val onClick: (HomeCategoryResponse) -> Unit) : RecyclerView.Adapter<DocumentAdapter.HomeCategoryViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): HomeCategoryViewHolder {
+        val binding: ViewDataBinding = DataBindingUtil.inflate(LayoutInflater.from(parent?.context), R.layout.item_home_category, parent, false)
+        return HomeCategoryViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: HomeCategoryViewHolder?, position: Int) {
+        holder?.itemView?.rippleDownload?.setOnRippleCompleteListener {
+            onClick(listHomePage[position])
+        }
+        holder?.bindItem(listHomePage[position])
+    }
+
+    override fun getItemCount(): Int {
+        return listHomePage.size
+    }
+
+    class HomeCategoryViewHolder(itemView: ViewDataBinding) : RecyclerView.ViewHolder(itemView.root) {
+        fun bindItem(homeCategoryResponse: HomeCategoryResponse) {
+            itemView.tvName.text = homeCategoryResponse.name
+        }
+    }
+}
--- app/src/main/java/haiphat/com/bds/nghetuvan/view/home/fragment/FinancialPackageFragment.kt
+++ app/src/main/java/haiphat/com/bds/nghetuvan/view/home/fragment/FinancialPackageFragment.kt
@@ -1,27 +1,54 @@
 package haiphat.com.bds.nghetuvan.view.home.fragment
 
 import android.databinding.DataBindingUtil
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.support.v4.widget.SwipeRefreshLayout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import haiphat.com.bds.nghetuvan.R
 import haiphat.com.bds.nghetuvan.databinding.FragmentFinancialPackageBinding
-import haiphat.com.bds.nghetuvan.databinding.FragmentNewsInfoBinding
 import haiphat.com.bds.nghetuvan.view.BaseFragment
-import haiphat.com.bds.nghetuvan.viewmodel.home.HomeInfoViewModel
-import org.sufficientlysecure.htmltextview.HtmlHttpImageGetter
 
 /**
  * Created by HUONG HA^P on 3/27/2018.
  */
-class FinancialPackageFragment : BaseFragment() {
+class FinancialPackageFragment : BaseFragment() , SwipeRefreshLayout.OnRefreshListener {
+
     private lateinit var dataBindingFragmentFinancialPackage: FragmentFinancialPackageBinding
+    private val urlWeb = "https://nghemoigioi.vn/goi-vay/cau-giay-center-point-goi-vay-ngan-hang-vietcombank/"
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         dataBindingFragmentFinancialPackage = DataBindingUtil.inflate(inflater, R.layout.fragment_financial_package, container, false)
+        dataBindingFragmentFinancialPackage.swipeRefreshLayout.setOnRefreshListener(this)
+        dataBindingFragmentFinancialPackage.swipeRefreshLayout.isRefreshing = true
+        loadData()
         return dataBindingFragmentFinancialPackage.root
     }
 
+    private fun loadData(){
+        dataBindingFragmentFinancialPackage.webFinancialPackage.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url)
+                return true
+            }
+
+            override fun onPageFinished(view: WebView?, url: String?) {
+                dataBindingFragmentFinancialPackage.swipeRefreshLayout.isRefreshing = false
+                super.onPageFinished(view, url)
+            }
+
+        }
+        dataBindingFragmentFinancialPackage.webFinancialPackage.loadUrl(urlWeb)
+
+    }
+
+    override fun onRefresh() {
+        dataBindingFragmentFinancialPackage.swipeRefreshLayout.isRefreshing = true
+        loadData()
+    }
 
     companion object {
         fun newInstance(content: String?, arguments: Bundle? = null): FinancialPackageFragment {
