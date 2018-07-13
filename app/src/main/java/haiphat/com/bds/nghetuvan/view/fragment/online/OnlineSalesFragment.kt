18572b91056e58741384ebe0ea62ff423f53f023
sivannguyen1991 <sivannguyen1991@gmail.com>
2018-07-13 09:08:56 +0700

    update code

--- app/src/main/java/haiphat/com/bds/nghetuvan/view/fragment/online/OnlineSalesFragment.kt
+++ app/src/main/java/haiphat/com/bds/nghetuvan/view/fragment/online/OnlineSalesFragment.kt
@@ -8,20 +8,42 @@ import android.view.View
 import android.view.ViewGroup
 import haiphat.com.bds.nghetuvan.R
 import haiphat.com.bds.nghetuvan.databinding.FragmentOnlineSalesBinding
+import haiphat.com.bds.nghetuvan.utils.dialog.ShowAlert
+import haiphat.com.bds.nghetuvan.utils.dialog.ShowLoading
 import haiphat.com.bds.nghetuvan.view.BaseFragment
 import haiphat.com.bds.nghetuvan.view.home.HomeActivity
+import haiphat.com.bds.nghetuvan.viewmodel.onlineSales.OnlineSalesViewModel
 
 class OnlineSalesFragment : BaseFragment(){
 
     private lateinit var dataBindingFragmentOnlineSales: FragmentOnlineSalesBinding
-
+    private var onlineSalesViewModel = OnlineSalesViewModel()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         dataBindingFragmentOnlineSales = DataBindingUtil.inflate(inflater, R.layout.fragment_online_sales, container, false)
         context?.let{
             (activity as HomeActivity).setBackgroundColor(ContextCompat.getColor(it, R.color.colorPrimary))
         }
+        getTypeTable()
         return dataBindingFragmentOnlineSales.root
     }
 
+    private fun getTypeTable(){
+        ShowLoading.show(context)
+        onlineSalesViewModel.getTypeTableOfGoods(onSuccess = {
+            ShowLoading.dismiss()
+        }, onFailed = {
+            ShowAlert.fail(pContext = context, message = it)
+            ShowLoading.dismiss()
+        })
+    }
 
+    private fun getTableGoods(){
+        ShowLoading.show(context)
+        onlineSalesViewModel.getTableOfGoods(onSuccess = {
+            ShowLoading.dismiss()
+        }, onFailed = {
+            ShowAlert.fail(pContext = context, message = it)
+            ShowLoading.dismiss()
+        })
+    }
 }
