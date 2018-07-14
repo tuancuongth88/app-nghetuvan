b93ee0aab22ee86bb1fff650c8b910bde16b0878
sivannguyen1991 <sivannguyen1991@gmail.com>
2018-07-14 09:21:56 +0700

    update code

--- app/src/main/java/haiphat/com/bds/nghetuvan/view/fragment/online/OnlineSalesFragment.kt
+++ app/src/main/java/haiphat/com/bds/nghetuvan/view/fragment/online/OnlineSalesFragment.kt
@@ -1,6 +1,5 @@
 package haiphat.com.bds.nghetuvan.view.fragment.online
 
-import android.app.Dialog
 import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v4.content.ContextCompat
