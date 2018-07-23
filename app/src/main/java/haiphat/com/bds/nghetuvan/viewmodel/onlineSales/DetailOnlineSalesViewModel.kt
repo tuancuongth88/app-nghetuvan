639da3fa93b00b91c2288f526a80465e2d9a71e5
sivannguyen1991 <sivannguyen1991@gmail.com>
2018-07-23 11:38:57 +0700

    update code finsh activity

--- app/src/main/java/haiphat/com/bds/nghetuvan/viewmodel/onlineSales/DetailOnlineSalesViewModel.kt
+++ app/src/main/java/haiphat/com/bds/nghetuvan/viewmodel/onlineSales/DetailOnlineSalesViewModel.kt
@@ -8,15 +8,14 @@ class DetailOnlineSalesViewModel {
         onSuccess(mockDataInfoOnlineSales())
     }
 
-
     private fun mockDataInfoOnlineSales(): ArrayList<TypeTableOfGoodsResponse> {
         var data = ArrayList<TypeTableOfGoodsResponse>()
-        data.add(TypeTableOfGoodsResponse(id = "1", name = "Hướng cửa"))
-        data.add(TypeTableOfGoodsResponse(id = "2", name = "Hướng ban công "))
-        data.add(TypeTableOfGoodsResponse(id = "3", name = "Số phòng ngủ"))
-        data.add(TypeTableOfGoodsResponse(id = "3", name = "Nhà vệ sinh"))
-        data.add(TypeTableOfGoodsResponse(id = "3", name = "diện tích tường"))
-        data.add(TypeTableOfGoodsResponse(id = "3", name = "Diện tích thông thúy"))
+        data.add(TypeTableOfGoodsResponse(id = "1", name = "Hướng cửa: TN"))
+        data.add(TypeTableOfGoodsResponse(id = "2", name = "Hướng ban công: DN "))
+        data.add(TypeTableOfGoodsResponse(id = "3", name = "Số phòng ngủ: 2"))
+        data.add(TypeTableOfGoodsResponse(id = "3", name = "Nhà vệ sinh: 1"))
+        data.add(TypeTableOfGoodsResponse(id = "3", name = "diện tích tường: 50m/2"))
+        data.add(TypeTableOfGoodsResponse(id = "3", name = "Diện tích thông thúy: 60m/2"))
         return data
     }
 
