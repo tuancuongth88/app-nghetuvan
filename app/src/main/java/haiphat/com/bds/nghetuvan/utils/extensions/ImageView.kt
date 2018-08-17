674910436d724e9d3c402a5907ef2f3699ae5696
sivannguyen1991 <sivannguyen1991@gmail.com>
2018-08-17 16:49:14 +0700

    update code

--- app/src/main/java/haiphat/com/bds/nghetuvan/utils/extensions/ImageView.kt
+++ app/src/main/java/haiphat/com/bds/nghetuvan/utils/extensions/ImageView.kt
@@ -2,12 +2,13 @@ package haiphat.com.bds.nghetuvan.utils.extensions
 
 import android.widget.ImageView
 import com.squareup.picasso.Picasso
+import haiphat.com.bds.nghetuvan.services.Config
 
 /**
  * Created by HUONG HA^P on 3/28/2018.
  */
 fun ImageView.fromUrl(url: String?, placeHolder: Int? = null) {
-    var request = Picasso.with(this.context).load("http://nghetuvan.cf" + url)
+    var request = Picasso.with(this.context).load(Config.API_URL_OAUTH + url)
     placeHolder?.let {
         request = request.placeholder(it)
     }
