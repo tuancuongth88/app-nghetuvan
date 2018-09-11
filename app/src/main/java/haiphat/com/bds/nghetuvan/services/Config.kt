0864697f17d681b90dec5d0e85dd9de4aad9f990
sivannguyen1991 <sivannguyen1991@gmail.com>
2018-09-11 17:43:13 +0700

    update api education

--- app/src/main/java/haiphat/com/bds/nghetuvan/services/Config.kt
+++ app/src/main/java/haiphat/com/bds/nghetuvan/services/Config.kt
@@ -6,7 +6,8 @@ class Config {
         const val API_URL_OAUTH = "http://app-bds.haiphatland.com.vn"
         const val API_URL_MEDIA ="http://media.haiphatland.com.vn/api"
         const val API_URL_CRM ="http://quanlyhocvien.ml/api"
-        const val API_URL_HPACADEMY ="http://hpacademy.com.vn/api"
+//        const val API_URL_HPACADEMY ="http://hpacademy.com.vn/api"
+        const val API_URL_HPACADEMY ="http://192.168.0.107:8000/api"
 
         const val CLIENT_ID = "2"
         const val CLIENT_SECRET = "tbSp7MgFTTYGPBxYILmTAofarMBeZuO9jo2EuLL3"
